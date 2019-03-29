6ce76943704a8f52e6b8532f25c80b0279fc1b0e
sniperHW <sniperHW@163.com>
2019-03-29 08:31:22 +0800

    none

--- MedianofTwoSortedArrays.cpp
+++ MedianofTwoSortedArrays.cpp
@@ -38,7 +38,7 @@ public:
 		return b;
 	}
 
-	double findMedianSortedArrays1(int *nums1,size_t len1,int *nums2,size_t len2,size_t need) {
+	double findKOfTwoSortedArrays(int *nums1,size_t len1,int *nums2,size_t len2,size_t need) {
         if(!nums1 || len1 == 0) {
             return nums2[need];
         }
@@ -51,32 +51,28 @@ public:
             if(need == 0 || need == 1){
                 return nums1[0];
             } else{
-                return findMedianSortedArrays1(&nums1[1],len1-1,&nums2[1],len2-1,need-2);
+                return findKOfTwoSortedArrays(&nums1[1],len1-1,&nums2[1],len2-1,need-2);
             }
         } else if(nums1[0] > nums2[0]) {
             int idx = binarySearch(nums1,len1,nums2[0]);
             if(idx == need){
                 return nums2[0];
             } else if(need > idx) {
-                return findMedianSortedArrays1(&nums1[idx],len1 - idx,&nums2[1],len2 - 1,need - idx - 1);
+                return findKOfTwoSortedArrays(&nums1[idx],len1 - idx,&nums2[1],len2 - 1,need - idx - 1);
             } else {
-                return findMedianSortedArrays1(NULL,0,nums2,len2,need);
+                return findKOfTwoSortedArrays(NULL,0,nums2,len2,need);
             }
         } else {
             int idx = binarySearch(nums2,len2,nums1[0]);
             if(idx == need){
                 return nums1[0];
             } else if(need > idx) {
-                return findMedianSortedArrays1(&nums1[1],len1 - 1,&nums2[idx],len2 - idx,need - idx - 1);
+                return findKOfTwoSortedArrays(&nums1[1],len1 - 1,&nums2[idx],len2 - idx,need - idx - 1);
             } else {
-                return findMedianSortedArrays1(nums1,len1,NULL,0,need);
+                return findKOfTwoSortedArrays(nums1,len1,NULL,0,need);
             }           
         }
 	}
-
-    double findMedianSortedArrays2(int *nums1,size_t len1,int *nums2,size_t len2,size_t need1,size_t need2) {
-        return (findMedianSortedArrays1(nums1,len1,nums2,len2,need1) + findMedianSortedArrays1(nums1,len1,nums2,len2,need2))/2;        
-    }
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         size_t len = nums1.size() + nums2.size();
@@ -88,9 +84,9 @@ public:
 
 
         if(mod == 0) {
-            return this->findMedianSortedArrays2(v1,nums1.size(),v2,nums2.size(),m-1,m);
+            return (findKOfTwoSortedArrays(v1,nums1.size(),v2,nums2.size(),m-1) + findKOfTwoSortedArrays(v1,nums1.size(),v2,nums2.size(),m))/2; 
         } else {
-            return this->findMedianSortedArrays1(v1,nums1.size(),v2,nums2.size(),m);
+            return findKOfTwoSortedArrays(v1,nums1.size(),v2,nums2.size(),m);
         }
     }
 };
@@ -107,5 +103,5 @@ int main(){
     v2.push_back(3);
 
 
-    printf("%f\n",s.findMedianSortedArrays1(&v1[0],v1.size(),&v2[0],v2.size(),1));
+    printf("%f\n",s.findKOfTwoSortedArrays(&v1[0],v1.size(),&v2[0],v2.size(),1));
 }
