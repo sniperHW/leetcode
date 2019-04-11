45e7d6bbd41d689ff962a81ef6420e4a10f0a197
huangwei <sniperhw@163.com>
2019-04-11 10:31:19 +0800

    none

--- SearchinRotatedSortedArray.cpp
+++ SearchinRotatedSortedArray.cpp
@@ -3,6 +3,72 @@
 
 using namespace std;
 
+
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+
+7 0 1 2 3 4 5 6
+
+1 2 3 4 5 6 7 0
+
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+
+*/
+
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==0) return -1;
+        
+        int start=0,end=nums.size()-1;
+        
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            
+            if(target==nums[mid]) return mid;
+            if(nums[mid]>nums[start]){
+                if(nums[start]<=target && target<=nums[mid]){
+                    end=mid;
+                }else{
+                    start=mid;
+                }
+            }
+            if(nums[mid]<nums[start]){
+                if(nums[mid]<=target && target<=nums[end]){
+                    start=mid;
+                }else{
+                    end=mid;
+                }
+            }
+        }
+        if(nums[start]==target) return start;
+        else if(nums[end]==target) return end;
+        else return -1;
+    }
+};
+
+*/
+
+
 class Solution {
 public:
 
